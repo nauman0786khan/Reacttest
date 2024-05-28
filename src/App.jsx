7101625db80e01212a0187ebda8bf81b626cb259
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-05-28 09:25:53 +0000

    done rect

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer";
+
+const App = ()=>{
+  const acc=()=>{
+
+alert("helo")
+
+  }
+  acc();
+
+  return(
+    <div>
+      <Footer/>
+    </div>
+  
+  )
+}
+export default App ;
