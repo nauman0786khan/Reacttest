ad27efbcec59dd89eb9530e18527a9a8bb2a0e44
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-05-30 08:10:43 +0000

    try accori

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,55 @@
 import Footer from "./components/Footer";
 
 const App = ()=>{
-  const acc=()=>{
+ 
+  const ac= ()=>{
+    const acc =document.querySelectorAll(".main");
+    console.log(acc)
 
-alert("helo")
 
   }
-  acc();
-
+  
+  ac();
   return(
+
+
     <div>
+<section className="main">
+<div className="accord">
+  <h1>this is heading</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsum, et animi totam, molestiae, itaque fuga dignissimos obcaecati sed enim similique? Dolore quis enim earum, nisi voluptas perferendis voluptate eveniet.
+  Beatae placeat necessitatibus corporis suscipit, impedit enim culpa consectetur repellat quo facere animi. Doloremque omnis suscipit neque facere beatae, itaque totam ullam blanditiis consequuntur, quisquam inventore voluptatibus, fuga est labore!
+  Adipisci repellat quidem quas odit magni maiores, reprehenderit a corporis ipsam dolor architecto ipsum et harum itaque aperiam soluta? Ratione adipisci quo saepe, excepturi natus aperiam nisi praesentium enim aut.
+  Dolores ipsum, quaerat, distinctio placeat sint vero iure maxime perspiciatis expedita autem tempore odit architecto sed ex officiis animi laudantium? Voluptate ut quasi iure iusto quidem. Itaque, natus? Nesciunt, corporis!</p>
+</div>
+<div className="accord">
+  <h1>this is heading</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsum, et animi totam, molestiae, itaque fuga dignissimos obcaecati sed enim similique? Dolore quis enim earum, nisi voluptas perferendis voluptate eveniet.
+  Beatae placeat necessitatibus corporis suscipit, impedit enim culpa consectetur repellat quo facere animi. Doloremque omnis suscipit neque facere beatae, itaque totam ullam blanditiis consequuntur, quisquam inventore voluptatibus, fuga est labore!
+  Adipisci repellat quidem quas odit magni maiores, reprehenderit a corporis ipsam dolor architecto ipsum et harum itaque aperiam soluta? Ratione adipisci quo saepe, excepturi natus aperiam nisi praesentium enim aut.
+  Dolores ipsum, quaerat, distinctio placeat sint vero iure maxime perspiciatis expedita autem tempore odit architecto sed ex officiis animi laudantium? Voluptate ut quasi iure iusto quidem. Itaque, natus? Nesciunt, corporis!</p>
+</div>
+<div className="accord">
+  <h1>this is heading</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsum, et animi totam, molestiae, itaque fuga dignissimos obcaecati sed enim similique? Dolore quis enim earum, nisi voluptas perferendis voluptate eveniet.
+  Beatae placeat necessitatibus corporis suscipit, impedit enim culpa consectetur repellat quo facere animi. Doloremque omnis suscipit neque facere beatae, itaque totam ullam blanditiis consequuntur, quisquam inventore voluptatibus, fuga est labore!
+  Adipisci repellat quidem quas odit magni maiores, reprehenderit a corporis ipsam dolor architecto ipsum et harum itaque aperiam soluta? Ratione adipisci quo saepe, excepturi natus aperiam nisi praesentium enim aut.
+  Dolores ipsum, quaerat, distinctio placeat sint vero iure maxime perspiciatis expedita autem tempore odit architecto sed ex officiis animi laudantium? Voluptate ut quasi iure iusto quidem. Itaque, natus? Nesciunt, corporis!</p>
+</div>
+
+
+</section>
+
       <Footer/>
     </div>
+
+
   
   )
+
+  
+  
 }
+
 export default App ;
+
